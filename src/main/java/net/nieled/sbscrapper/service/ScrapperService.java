eeea7dc4db4a4094f28b54ba413e7cdc6416378b
Daniel Calle <nieled@riseup.net>
2020-12-06 19:15:20 -0500

    Retrieve and instantiate data from web

--- src/main/java/net/nieled/sbscrapper/service/ScrapperService.java
+++ src/main/java/net/nieled/sbscrapper/service/ScrapperService.java
@@ -1,14 +1,44 @@
 package net.nieled.sbscrapper.service;
 
 import net.nieled.sbscrapper.domain.Entry;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ScrapperService {
 
     private static final String URL = "https://news.ycombinator.com/";
 
-    public List<Entry> getData() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public List<Entry> getData() throws IOException {
+        Document doc = Jsoup.connect(URL).get();
+
+        List<Entry> entries = new ArrayList<>();
+        Elements titles = doc.select(".athing");
+        for (Element element : titles.subList(0, Math.min(30, titles.size()))) {
+            var details = element.nextElementSibling();
+
+            var orderNo = Integer.parseInt(element.child(0).text().replace(".", ""));
+            var title = element.child(2).text();
+            int comments = asNumber(details.select("a:nth-child(6)").text().replaceAll("\\D+", ""));
+            int points = asNumber(details.select(".score").text().replaceAll("\\D+", ""));
+
+            entries.add(new Entry(orderNo, title, comments, points));
+        }
+        return entries;
     }
+
+    protected int asNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+    }
+
 }
+
--- src/test/java/net/nieled/sbscrapper/service/ScrapperServiceTest.java
+++ src/test/java/net/nieled/sbscrapper/service/ScrapperServiceTest.java
@@ -7,10 +7,19 @@ import static org.junit.Assert.*;
 public class ScrapperServiceTest {
 
     @Test
-    public void entriesLength() {
+    public void entriesLength() throws Exception {
         ScrapperService scrapperService = new ScrapperService();
         var result = scrapperService.getData();
         assertTrue(result.size() <= 30);
+    }
+
+    @Test
+    public void numberConversion() {
+        ScrapperService scrapperService = new ScrapperService();
+        assertEquals(0, scrapperService.asNumber(""));
+        assertEquals(0, scrapperService.asNumber("hide"));
+        assertEquals(1, scrapperService.asNumber("1"));
+        assertEquals(42, scrapperService.asNumber("42"));
     }
 
 }
