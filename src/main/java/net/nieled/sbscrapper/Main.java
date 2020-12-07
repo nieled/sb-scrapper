59bd2fa1c60c00e907fe7914774d14f55f48bc4a
Daniel Calle <nieled@riseup.net>
2020-12-06 20:38:43 -0500

    Fill main method, prints to console

--- src/main/java/net/nieled/sbscrapper/Main.java
+++ src/main/java/net/nieled/sbscrapper/Main.java
@@ -1,9 +1,28 @@
 package net.nieled.sbscrapper;
 
+import net.nieled.sbscrapper.service.FiltersService;
+import net.nieled.sbscrapper.service.ScrapperService;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String... args) {
+        ScrapperService scrapperService = new ScrapperService();
+        FiltersService filtersService = new FiltersService();
+        try {
+            var data = scrapperService.getData();
 
+            var filter1 = filtersService.firstFilter(data);
+            var filter2 = filtersService.secondFilter(data);
+
+            System.out.println("First filter results:");
+            filter1.forEach(System.out::println);
+            System.out.println("Second filter results:");
+            filter2.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Oops! We got an error");
+        }
     }
 }
 
